a59b9cc6127c3959ed5d0bfe9dd37f28d64555e4
Jeric <jericclim@gmail.com>
2025-08-29 18:14:31 +0800

    feat: Add view recipefunction

--- core_api/serializers.py
+++ core_api/serializers.py
@@ -6,12 +6,21 @@ class IngredientSerializer(serializers.ModelSerializer):
         model = Ingredient
         fields = '__all__'
 
+class RecipeIngredientSerializer(serializers.ModelSerializer):
+    ingredient_name = serializers.ReadOnlyField(source='ingredient.name')
+
+    class Meta:
+        model = IngredientRecipe
+        fields = ['ingredient_name', 'required_quantity', 'unit_of_measure']
+
 class RecipeSerializer(serializers.ModelSerializer):
+    ingredients = RecipeIngredientSerializer(source='ingredientrecipe_set', many=True, read_only=True)
     class Meta:
         model = Recipe
-        fields = '__all__'
+        fields = ['id', 'name', 'description', 'batch_size', 'ingredients', 'date_added']
 
 class IngredientRecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = IngredientRecipe
         fields = '__all__'
+
