4cd452cb6144503a7b4e70b35d055c0663144bd1
Jeric <jericclim@gmail.com>
2025-08-29 21:03:19 +0800

    chore: Remove hardcoded SECRET_KEY and DEBUG from settings

--- inventory_project/settings.py
+++ inventory_project/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
+
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o+l2mpeugs8@0mu+##zhpq6*=^6fpbb800g2us0*7(7&lks4(w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
